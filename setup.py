eaea5437717b88628e4d78ee5e6d02904abb4299
bramvanuldenn <70747253+bramvanuldenn@users.noreply.github.com>
2022-10-31 15:49:04 +0100

    final try lol

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='Holmes',
     version='0.1',
-    packages=['src', 'src.jobs', 'src.objects', 'src.objects.relation', 'src.objects.data_objects',
-              'src.objects.data_objects.exceptions'],
+    py_modules=['Holmes'],
+    package_dir={'': 'src'},
     url='https://github.com/bramvanuldenn/Holmes',
     license='Apache',
     author='bramv',
---
+++ src/Holmes.py
@@ -0,0 +1,2 @@
+from src.jobs import *
+from src.objects import *
