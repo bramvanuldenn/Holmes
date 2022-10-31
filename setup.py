1ce0330f1b1a46bbf5feb2220932c9ef5c63fbd9
bramvanuldenn <70747253+bramvanuldenn@users.noreply.github.com>
2022-10-31 16:15:21 +0100

    added bson to install_requires

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=['Holmes', 'Holmes.jobs', 'Holmes.objects', 'Holmes.objects.relation', 'Holmes.data_objects',
               'Holmes.data_objects.exceptions'],
+    install_requires=['bson==0.5.10'],
     url='https://github.com/bramvanuldenn/Holmes',
     license='Apache',
     author='bramv',
